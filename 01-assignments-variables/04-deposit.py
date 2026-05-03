66acda9b31e5fef31b4ddc486a96bbec9125b685
niteshkal03 <niteshkalangada888@gmail.com>
2026-05-03 21:14:45 +0530

    new deposit file updated

---
+++ 01-assignments-variables/04-deposit.py
@@ -0,0 +1,11 @@
+# 5. Update balance after deposit 
+current_balance = 10000
+deposit_balance = 5000
+
+print(f"Before Deposit Current Balance is: {current_balance}")
+print(f"Amount for deposit is: {deposit_balance}")
+print("="*40)
+print()
+current_balance = current_balance + deposit_balance
+
+print(f"After Deposit the Current Balance is: {current_balance}")

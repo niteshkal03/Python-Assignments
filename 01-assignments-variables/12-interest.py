de23b8b80518f48518d09827c2468da7d244ab12
niteshkal03 <niteshkalangada888@gmail.com>
2026-05-04 16:30:11 +0530

    question no. 12 solved

---
+++ 01-assignments-variables/12-interest.py
@@ -0,0 +1,16 @@
+# 13. Calculate simple interest
+principal = 10000
+rate = 5 
+time = 2
+
+print(f"The Principle Amount is: {principal}")
+print(f"The Rate is: {rate}")
+print(f"The time is: {time}")
+print()
+
+simple_interest = (principal * rate * time) / 100
+print(f"The Simple Interest is: {simple_interest}")
+
+
+
+

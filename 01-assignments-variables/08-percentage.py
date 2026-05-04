dd58d69e90e6320dc81d28ec0fa2be45caac9b80
niteshkal03 <niteshkalangada888@gmail.com>
2026-05-04 11:22:28 +0530

    one new question solved

---
+++ 01-assignments-variables/08-percentage.py
@@ -0,0 +1,11 @@
+# 9. Calculate student percentage
+obtain_marks = 430
+out_of = 500
+
+print(f"Student's Obtained Marks: {obtain_marks}")
+print(f"Out of : {out_of}")
+
+percentage = int(obtain_marks / out_of * 100)
+
+print(f"Student's Percentage: {percentage} ")
+

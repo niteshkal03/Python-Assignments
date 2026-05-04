d70278cc654490224f24dc69df24952a0b509e58
niteshkal03 <niteshkalangada888@gmail.com>
2026-05-04 11:44:26 +0530

    one new question solved

--- 01-assignments-variables/07-discount.py
+++ 01-assignments-variables/07-discount.py
@@ -1,10 +1,10 @@
 # 8. Apply a 20% discount to a price
 price = 1000
-discount = 20
+discount_percent = 20
 print(f"Before Discount the Price is (Rs.): {price}")
-print(f"The Discount is: {discount}%")
+print(f"The Discount is: {discount_percent}%")
 print()
-discount = (price * discount)/100
+discount = (price * discount_percent)/100
 discount_price = price - discount
 
 print(f"After Discount The Price is (Rs.): {discount_price}")
---
+++ 01-assignments-variables/09-average.py
@@ -0,0 +1,25 @@
+# 10. Calculate total and average of 4 subjects 
+
+subject1 = 80 
+subject2 = 75
+subject3 = 90 
+subject4 = 85
+
+total_subjects = 4
+
+print(f"Student's Mark:")
+print(f"subject1 = {subject1}")
+print(f"subject2 = {subject2}")
+print(f"subject3 = {subject3}")
+print(f"subject4 = {subject4}")
+print()
+
+total_marks = subject1 + subject2 + subject3 + subject4
+print(f"Total Marks of 4 Subjects: {total_marks}")
+print()
+print("="*35)
+average = total_marks / total_subjects
+print(f"Average of 4 Subject: {average}")
+
+ 
+

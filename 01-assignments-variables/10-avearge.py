f7f2686fd29b9db20ad98c62b0b920dbd4aa9bb7
niteshkal03 <niteshkalangada888@gmail.com>
2026-05-04 12:09:24 +0530

    question no. 10 is solved

---
+++ 01-assignments-variables/10-avearge.py
@@ -0,0 +1,16 @@
+# 11. Calculate average of 3 numbers 
+num1 = 25 
+num2 = 35 
+num3 = 45 
+
+print(f"Average of 3 Numbers are:")
+print(f"""num1: {num1}
+num2: {num2}
+num3: {num3}
+""")
+
+total = num1 + num2 + num3
+print(f"Total of Numbers are: {total}")
+
+average = total / 3
+print(f"Average of 3 Numbers is: {average}")

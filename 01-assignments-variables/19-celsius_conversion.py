ff79f05fa5c527905f926549090af3152579e19d
niteshkal03 <niteshkalangada888@gmail.com>
2026-05-05 20:13:49 +0530

    20th Question Solved

--- 01-assignments-variables/19-celsius_conversion.py
+++ 01-assignments-variables/19-celsius_conversion.py
@@ -1,6 +1,9 @@
 # 20. Convert 50°F to Celsius
 # formula: celsius = (fehrenheit - 32) * 5 / 9
 fahrenheit = 50
+print(f"Before Conversion Fahrenheit (°F): {fahrenheit}")
+print("="*40)
+print()
 
 celsius = (fahrenheit - 32) * (5 / 9)
-print(celsius)
+print(f"After Conversion Celsius(°C): {celsius}")
---
+++ 01-assignments-variables/20-binary_conversion.py
@@ -0,0 +1,9 @@
+# 21. Convert integer to binary 
+int_val = 25
+print(f"Before Conversion the Integer Value: {int_val}")
+print("="*40)
+print()
+
+bin_val = bin(int_val)
+print(f"After Conversion the Binary Value: {bin_val}")
+
